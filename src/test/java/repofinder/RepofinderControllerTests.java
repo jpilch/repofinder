c4c3ee36ee9618ce395c6bd56bc412ece0a04d49
Jan Pilch <pilch.jan.kontakt@gmail.com>
2023-09-01 14:49:28 +0200

    test(RepofinderController): fix broken listReposFor_UnsupportedContentType

--- src/test/java/repofinder/RepofinderControllerTests.java
+++ src/test/java/repofinder/RepofinderControllerTests.java
@@ -1,5 +1,6 @@
 package repofinder;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +10,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.HttpClientErrorException;
 import repofinder.controller.RepofinderController;
+import repofinder.model.GithubRepository;
 import repofinder.service.GithubService;
 
 import java.util.List;
@@ -41,46 +43,50 @@ public class RepofinderControllerTests {
 //        mockMvc.perform(get("/bob")
 //            .accept("application/json"))
 //            .andExpect(status().isNotFound())
-//            .andExpect(jsonPath("$.message")
-//                .value("404 User not found"))
+//            .andExpect(jsonPath("$.Message")
+//                .value(anyMap()))
 //            .andExpect(jsonPath("$.status").value(404));
 //
 //        verify(githubService).findAllReposFor(any(String.class));
 //    }
-//
-//    @Test
-//    public void listReposFor_UnsupportedContentType() throws Exception {
-//        given(githubService.findAllReposFor(any(String.class)))
-//            .willReturn(List.of());
-//
-//        mockMvc.perform(get("/bob")
-//                .accept("application/xml"))
-//            .andExpect(status().isNotAcceptable())
-//            .andExpect(jsonPath("$.message")
-//                .value("No acceptable representation"))
-//            .andExpect(jsonPath("$.status").value(406));
-//
-//        verify(githubService, times(0))
-//            .findAllReposFor(any(String.class));
-//    }
 
-//    @Test
-//    public void listReposFor_ValidUser() throws Exception {
-//        Repository.Branch mockBranch = new Repository.Branch("master", "00f99f711380bd6c");
-//        Repository mockRepository = new Repository("john", "repofinder", List.of(mockBranch));
-//
-//        given(githubService.findAllReposFor("john"))
-//            .willReturn(List.of(mockRepository));
-//
-//        mockMvc.perform(get("/john")
-//            .accept("application/json"))
-//            .andExpect(status().isOk())
-//            .andExpect(jsonPath("$[0].ownerLogin").value("john"))
-//            .andExpect(jsonPath("$[0].name").value("repofinder"))
-//            .andExpect(jsonPath("$[0].branches[0].name").value("master"))
-//            .andExpect(jsonPath("$[0].branches[0].commitSha").value("00f99f711380bd6c"));
-//
-//        verify(githubService).findAllReposFor("john");
-//    }
+    @Test
+    public void listReposFor_UnsupportedContentType() throws Exception {
+        given(githubService.findAllReposFor(any(String.class)))
+            .willReturn(List.of());
+
+        mockMvc.perform(get("/bob")
+                .accept("application/xml"))
+            .andExpect(status().isNotAcceptable())
+            .andExpect(jsonPath("$.Message")
+                .value("No acceptable representation"))
+            .andExpect(jsonPath("$.status").value(406));
+
+        verify(githubService, times(0))
+            .findAllReposFor(any(String.class));
+    }
+
+    @Test
+    public void listReposFor_ValidUser() throws Exception {
+        GithubRepository.Branch mockBranch = new GithubRepository.Branch("master", "00f99f711380bd6c");
+        GithubRepository mockRepository = new GithubRepository(
+            "john",
+            "repofinder",
+            false,
+            List.of(mockBranch));
+
+        given(githubService.findAllReposFor("john"))
+            .willReturn(List.of(mockRepository));
+
+        mockMvc.perform(get("/john")
+            .accept("application/json"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].ownerLogin").value("john"))
+            .andExpect(jsonPath("$[0].name").value("repofinder"))
+            .andExpect(jsonPath("$[0].branches[0].name").value("master"))
+            .andExpect(jsonPath("$[0].branches[0].commitSha").value("00f99f711380bd6c"));
+
+        verify(githubService).findAllReposFor("john");
+    }
 
 }
