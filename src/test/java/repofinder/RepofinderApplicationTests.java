e0a2059a810e135f5e1896d8b4fec88df6292bb7
Jan Pilch <pilch.jan.kontakt@gmail.com>
2023-09-02 18:47:15 +0200

    test(RepofinderApplicationTests): improve identifiesCachedResultsByUsername test case

--- src/test/java/repofinder/RepofinderApplicationTests.java
+++ src/test/java/repofinder/RepofinderApplicationTests.java
@@ -8,14 +8,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.cache.Cache;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.test.annotation.DirtiesContext;
+import repofinder.model.GithubRepository;
 import repofinder.service.GithubService;
 
 import java.util.List;
 
 import static org.mockito.BDDMockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RepofinderApplicationTests {
 
 	@Autowired
@@ -51,11 +52,17 @@ class RepofinderApplicationTests {
 
 	@Test
 	void identifiesCachedResultsByUsername() throws Exception {
-		given(githubService.findAllNonForkReposFor("john")).willReturn(List.of());
+		GithubRepository repo1 = new GithubRepository("bob", "test", List.of(), false);
+		GithubRepository repo2 = new GithubRepository("alice", "test", List.of(), false);
+		given(githubService.findAllNonForkReposFor("bob"))
+			.willReturn(List.of(repo1));
+		given(githubService.findAllNonForkReposFor("alice"))
+			.willReturn(List.of(repo2));
 
-		testRestTemplate.getForObject("/bob", String.class);
-		testRestTemplate.getForObject("/alice", String.class);
+		String serializedRepo1 = testRestTemplate.getForObject("/bob", String.class);
+		String serializedRepo2 = testRestTemplate.getForObject("/alice", String.class);
 
 		verify(githubService, times(2)).findAllNonForkReposFor(anyString());
+		assertNotEquals(serializedRepo1, serializedRepo2);
 	}
 }
