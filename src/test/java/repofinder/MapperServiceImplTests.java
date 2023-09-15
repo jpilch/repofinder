97d0d8c2f122ea1170b5c6895c4d8e55fa848b30
Jan Pilch <jan.pilch.dev@gmail.com>
2023-09-15 12:47:38 +0200

    test(MapperServiceImpl): add mapsGithubRepository test case

--- src/test/java/repofinder/MapperServiceImplTests.java
+++ src/test/java/repofinder/MapperServiceImplTests.java
@@ -3,8 +3,14 @@ package repofinder;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import repofinder.model.Repository;
+import repofinder.model.api.GithubRepository;
 import repofinder.service.MapperService;
 import repofinder.service.MapperServiceImpl;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MapperServiceImplTests {
 
@@ -15,6 +21,22 @@ public class MapperServiceImplTests {
         mapperService = new MapperServiceImpl();
     }
 
-//    @Test
-//    public void correctly
+    @Test
+    public void mapsGithubRepository() throws Exception {
+        GithubRepository mockRepo = new GithubRepository(
+                "repofinder",
+                new GithubRepository.Owner("john"),
+                false
+        );
+        List<Repository.Branch> preparedBranches = List.of(
+                new Repository.Branch("master", "8d88330f")
+        );
+
+        Repository result = mapperService.mapRepository(mockRepo, preparedBranches);
+
+        assertEquals(result.name(), mockRepo.name());
+        assertEquals(result.ownerLogin(), mockRepo.owner().login());
+        assertEquals(result.branches().size(), preparedBranches.size());
+        assertEquals(result.branches().get(0), preparedBranches.get(0));
+    }
 }
