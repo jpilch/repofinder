462e25d127477538f59ba5575a8b48bdf898a01b
Jan Pilch <jan.pilch.dev@gmail.com>
2023-09-16 09:22:23 +0200

    test(RepofinderApplication): configure mock server using wire mock

--- src/test/java/repofinder/RepofinderApplicationTests.java
+++ src/test/java/repofinder/RepofinderApplicationTests.java
@@ -1,12 +1,36 @@
 package repofinder;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RepofinderApplicationTests {
 
+	@RegisterExtension
+	static WireMockExtension mockServer = WireMockExtension.newInstance()
+			.options(wireMockConfig().dynamicPort().dynamicHttpsPort())
+			.build();
+
+	@DynamicPropertySource
+	static void configureMockServer(DynamicPropertyRegistry registry) {
+		registry.add("github.client.config.rootUri", mockServer::baseUrl);
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@Disabled
+	public void shouldGetRepositoriesForGivenUsername() throws Exception {
+
 	}
 }
